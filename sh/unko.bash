a28fcaf755eaa6210f55116a015d7d09ce16d741
lninyu <lninyurir@email.com>
2021-02-09 04:04:46 +0000

    Tue Feb  9 04:04:46 UTC 2021

--- sh/unko.bash
+++ sh/unko.bash
@@ -1,4 +1,4 @@
 cd $(dirname $0)/../
 git add ./
-echo $(git commit -m "$(date)"|head -n 2|tail -n 1) | tee -a hogyaaa.txt
+echo $(date):$(git commit -m "$(date)"|head -n 2|tail -n 1) | tee -a hogyaaa.txt
 git push origin ugaaaaaa
