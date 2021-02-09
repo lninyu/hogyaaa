0f5f4db4e9d48f796fb06fe6bf63a205399664ae
lninyu <lninyurir@email.com>
2021-02-09 03:49:44 +0000

    Tue Feb  9 03:49:44 UTC 2021

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 べんきょうよう
 するとはいってない
+ 1 file changed, 1 insertion(+), 1 deletion(-)
--- sh/unko.bash
+++ sh/unko.bash
@@ -1,4 +1,4 @@
 cd $(dirname $0)/../
 git add ./
-git commit -m "$(date)"|tee|head -n 2|tail -n 1 >> README.md
+git commit -m "$(date)"|head -n 2|tail -n 1 >> README.md
 git push origin ugaaaaaa
